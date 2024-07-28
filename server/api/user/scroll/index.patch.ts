998470859eee5d16de979f75b5cce7dbeda8480b
Charley Reeson <neon.fortytwo@gmail.com>
2024-07-28 21:44:37 +0100

    it should be a transaction

--- server/api/user/scroll/index.patch.ts
+++ server/api/user/scroll/index.patch.ts
@@ -8,21 +8,25 @@ export default defineEventHandler(async (event) => {
     readBody<string[]>(event),
   ]);
 
-  // delete
-  await pool.execute<RowDataPacket[]>(
-    `DELETE FROM hatchery WHERE user_id = ?`,
-    [token?.userId]
-  );
+  const con = await pool.getConnection();
+  await con.beginTransaction();
+
+  await con.execute<RowDataPacket[]>(`DELETE FROM hatchery WHERE user_id = ?`, [
+    token?.userId,
+  ]);
 
   // insert only if dragons were selected
   if (body.length > 0) {
-    const bulkInsert = pool.format(
+    const bulkInsert = con.format(
       `INSERT INTO hatchery (code, user_id) VALUES ?`,
       [body.map((id: string) => [id, token?.userId])]
     );
 
-    await pool.execute<RowDataPacket[]>(bulkInsert);
+    await con.execute<RowDataPacket[]>(bulkInsert);
   }
+
+  await con.commit();
+  con.release();
 
   return body;
 });
