6d6518247ff4ff78d34398526e91b22da743cbba
Charley Reeson <neon.fortytwo@gmail.com>
2024-08-01 22:30:45 +0100

    fix: my own cleaning tool

--- server/api/hatchery/index.delete.ts
+++ server/api/hatchery/index.delete.ts
@@ -1,10 +1,13 @@
-import { getServerSession } from "next-auth";
+import { getServerSession } from "#auth";
+import { cleanUp } from "~/server/clean-up";
 
 export default defineEventHandler(async (event) => {
-  const session = await getServerSession();
+  const session = await getServerSession(event);
 
   if (session?.user.role !== "owner") {
     setResponseStatus(event, 401, "Unauthorized");
     return "Unauthorized";
   }
+
+  return cleanUp();
 });
