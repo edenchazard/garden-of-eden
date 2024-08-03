fac59fe40008cfa08611405050acd4f931f87cca
Charley Reeson <neon.fortytwo@gmail.com>
2024-08-03 14:20:05 +0100

    fix: maybe even validate user settings too lol

--- server/api/user/settings/index.patch.ts
+++ server/api/user/settings/index.patch.ts
@@ -1,6 +1,7 @@
 import pool from "~/server/pool";
 import type { RowDataPacket } from "mysql2";
 import { getToken } from "#auth";
+import { z } from "zod";
 
 export default defineEventHandler(async (event) => {
   const [token, body] = await Promise.all([
@@ -8,11 +9,19 @@ export default defineEventHandler(async (event) => {
     readBody<Partial<UserSettings>>(event),
   ]);
 
+  const settings = z
+    .object({
+      frequency: z.coerce.string().optional(),
+      perPage: z.coerce.number().optional(),
+      sort: z.coerce.string().optional(),
+    })
+    .parse(body);
+
   // delete
   await pool.execute<RowDataPacket[]>(
     `UPDATE user_settings SET frequency = ?, perPage = ?, sort = ? WHERE user_id = ?`,
     [
-      body.frequency ?? null,
+      settings.frequency ?? null,
       body.perPage ?? null,
       body.sort ?? null,
       token?.userId,
