4cbb805fd91fe38ecf6740a6db737ac5ca734f1a
Charley Reeson <neon.fortytwo@gmail.com>
2024-08-03 21:46:21 +0100

    rf

--- app.vue
+++ app.vue
@@ -23,7 +23,7 @@
               >
                 <font-awesome-icon
                   :icon="['fas', 'broom']"
-                  class="!mr-0 !"
+                  class="!mr-0"
                 />
               </button>
               <label class="inline-flex items-center cursor-pointer">
@@ -144,116 +144,13 @@
                     gridTemplateColumns: `repeat(auto-fill, minmax(17rem, 1fr))`,
                   }"
                 >
-                  <div
-                    v-for="dragon in dragons"
+                  <ScrollPanel
+                    v-for="(dragon, i) in dragons"
                     :key="dragon.id"
+                    :recently-added
+                    v-model="dragons[i]"
                     @click="dragon.inHatchery = !dragon.inHatchery"
-                    class="relative cursor-pointer transition-shadow"
-                    :class="{
-                      'opacity-50': !dragon.inHatchery,
-                      'shadow-[0px_0px_15px_1px] shadow-yellow-600':
-                        recentlyAdded.includes(dragon.id),
-                    }"
-                  >
-                    <div
-                      class="grid grid-cols-[45px_1fr] gap-x-3 gap-y-2 p-2 pb-4 rounded-md items-center border content-border justify-items-start overflow-hidden"
-                      :class="
-                        dragon.inHatchery
-                          ? 'bg-green-500 dark:bg-neutral-800 border-transparent'
-                          : 'border-green-500 dark:border-neutral-600'
-                      "
-                    >
-                      <NuxtLink
-                        :to="`https://dragcave.net/view/${dragon.id}`"
-                        target="_blank"
-                        class="size-full flex items-center justify-center"
-                        :aria-labelledby="`dragon-${dragon.id}`"
-                        @click.stop
-                      >
-                        <img
-                          alt=""
-                          loading="lazy"
-                          class="max-w-full max-h-full"
-                          :src="`https://dragcave.net/image/${dragon.id}/1`"
-                        />
-                      </NuxtLink>
-                      <div class="text-left w-full">
-                        <span
-                          class="block truncate font-bold"
-                          :id="`dragon-${dragon.id}`"
-                        >
-                          <span :aria-hidden="!dragon.name">{{
-                            dragon.name ?? "Unnamed"
-                          }}</span>
-                          <span
-                            class="sr-only"
-                            :aria-hidden="!!dragon.name"
-                            >({{ dragon.id }})</span
-                          >
-                        </span>
-                        <span
-                          class="text-sm pl-3 italic"
-                          :aria-hidden="!dragon.name"
-                        >
-                          ({{ dragon.id }})
-                        </span>
-                      </div>
-
-                      <div
-                        class="uppercase text-xs bg-green-900/70 dark:bg-neutral-950/70 p-1 rounded-md self-end *:underline-offset-2 absolute right-3 bottom-4 px-1.5"
-                      >
-                        {{ formatNumber(dragon.views)
-                        }}<abbr title="Views">V</abbr> /
-                        {{ formatNumber(dragon.unique)
-                        }}<abbr title="Unique Views">U</abbr> /
-                        {{ formatNumber(dragon.clicks)
-                        }}<abbr title="Clicks">C</abbr>
-                      </div>
-                      <input
-                        :id="`dragon-check-${dragon.id}`"
-                        class="justify-self-end -top-2 -right-2 absolute"
-                        type="checkbox"
-                        v-model="dragon.inHatchery"
-                        :aria-labelledby="`dragon-${dragon.id}`"
-                      />
-                    </div>
-                    <div
-                      class="bg-green-900 dark:bg-neutral-950 dark:text-stone-400 divide-x divide-white dark:divide-stone-400 text-xs text-left px-2 py-0.5 rounded-md absolute -bottom-2 right-1 *:px-2"
-                    >
-                      <span
-                        class="!pl-0.5"
-                        title="Views to unique views ratio"
-                        >{{ formatRatio(dragon.views, dragon.unique) }}</span
-                      >
-                      <span>
-                        <NuxtLink
-                          v-if="dragon.parent_f && dragon.parent_m"
-                          class="text-white dark:text-stone-200 decoration-transparent"
-                          :to="`https://dragcave.net/lineage/${dragon.id}`"
-                          target="_blank"
-                          @click.stop
-                          >LINEAGE</NuxtLink
-                        >
-                        <span v-else>CB</span>
-                      </span>
-                      <span
-                        v-if="dragon.gender"
-                        :title="dragon.gender"
-                      >
-                        <font-awesome-icon
-                          :icon="['fas', 'mars']"
-                          v-if="dragon.gender === 'Male'"
-                        />
-                        <font-awesome-icon
-                          :icon="['fas', 'venus']"
-                          v-else-if="dragon.gender === 'Female'"
-                        />
-                      </span>
-                      <span class="!pr-0.5">
-                        {{ formatHoursLeft(dragon.hoursleft) }}
-                      </span>
-                    </div>
-                  </div>
+                  />
                 </div>
                 <ScrollToolbar
                   id="scroll-toolbar-2"
@@ -377,6 +274,8 @@ export default {
 };
 </script>
 <script setup lang="ts">
+import ScrollPanel from "~/components/ScrollPanel.vue";
+
 useHead({
   title: "Garden Of Eden",
 });
---
+++ components/ScrollPanel.vue
@@ -0,0 +1,115 @@
+<template>
+  <div
+    class="relative cursor-pointer transition-shadow"
+    :class="{
+      'opacity-50': !dragon.inHatchery,
+      'shadow-[0px_0px_15px_1px] shadow-yellow-600': recentlyAdded.includes(
+        dragon.id
+      ),
+    }"
+  >
+    <div
+      class="grid grid-cols-[45px_1fr] gap-x-3 gap-y-2 p-2 pb-4 rounded-md items-center border content-border justify-items-start overflow-hidden"
+      :class="
+        dragon.inHatchery
+          ? 'bg-green-500 dark:bg-neutral-800 border-transparent'
+          : 'border-green-500 dark:border-neutral-600'
+      "
+    >
+      <NuxtLink
+        :to="`https://dragcave.net/view/${dragon.id}`"
+        target="_blank"
+        class="size-full flex items-center justify-center"
+        :aria-labelledby="`dragon-${dragon.id}`"
+        @click.stop
+      >
+        <img
+          alt=""
+          loading="lazy"
+          class="max-w-full max-h-full"
+          :src="`https://dragcave.net/image/${dragon.id}/1`"
+        />
+      </NuxtLink>
+      <div class="text-left w-full">
+        <span
+          class="block truncate font-bold"
+          :id="`dragon-${dragon.id}`"
+        >
+          <span :aria-hidden="!dragon.name">{{
+            dragon.name ?? "Unnamed"
+          }}</span>
+          <span
+            class="sr-only"
+            :aria-hidden="!!dragon.name"
+            >({{ dragon.id }})</span
+          >
+        </span>
+        <span
+          class="text-sm pl-3 italic"
+          :aria-hidden="!dragon.name"
+        >
+          ({{ dragon.id }})
+        </span>
+      </div>
+
+      <div
+        class="uppercase text-xs bg-green-900/70 dark:bg-neutral-950/70 p-1 rounded-md self-end *:underline-offset-2 absolute right-3 bottom-4 px-1.5"
+      >
+        {{ formatNumber(dragon.views) }}<abbr title="Views">V</abbr> /
+        {{ formatNumber(dragon.unique) }}<abbr title="Unique Views">U</abbr> /
+        {{ formatNumber(dragon.clicks) }}<abbr title="Clicks">C</abbr>
+      </div>
+      <input
+        :id="`dragon-check-${dragon.id}`"
+        class="justify-self-end -top-2 -right-2 absolute"
+        type="checkbox"
+        v-model="dragon.inHatchery"
+        :aria-labelledby="`dragon-${dragon.id}`"
+      />
+    </div>
+    <div
+      class="bg-green-900 dark:bg-neutral-950 dark:text-stone-400 divide-x divide-white dark:divide-stone-400 text-xs text-left px-2 py-0.5 rounded-md absolute -bottom-2 right-1 *:px-2"
+    >
+      <span
+        class="!pl-0.5"
+        title="Views to unique views ratio"
+        >{{ formatRatio(dragon.views, dragon.unique) }}</span
+      >
+      <span>
+        <NuxtLink
+          v-if="dragon.parent_f && dragon.parent_m"
+          class="text-white dark:text-stone-200 decoration-transparent"
+          :to="`https://dragcave.net/lineage/${dragon.id}`"
+          target="_blank"
+          @click.stop
+          >LINEAGE</NuxtLink
+        >
+        <span v-else>CB</span>
+      </span>
+      <span
+        v-if="dragon.gender"
+        :title="dragon.gender"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'mars']"
+          v-if="dragon.gender === 'Male'"
+        />
+        <font-awesome-icon
+          :icon="['fas', 'venus']"
+          v-else-if="dragon.gender === 'Female'"
+        />
+      </span>
+      <span class="!pr-0.5">
+        {{ formatHoursLeft(dragon.hoursleft) }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  recentlyAdded: string[];
+}>();
+
+const dragon = defineModel<ScrollView>({ required: true });
+</script>
