b80599fa6e1bb84938430916c50d8e6a1527807f
Charley Reeson <neon.fortytwo@gmail.com>
2024-08-09 23:29:51 +0100

    fix: min perPage validation

--- utils/userSettingsSchema.ts
+++ utils/userSettingsSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export default z.object({
   frequency: z.coerce.string(),
-  perPage: z.coerce.number(),
+  perPage: z.coerce.number().min(10),
   sort: z.union([z.literal("Youngest First"), z.literal("Oldest First")]),
   hatchlingMinAge: z.coerce.number().max(72).min(0),
   eggMinAge: z.coerce.number().max(72).min(0),
