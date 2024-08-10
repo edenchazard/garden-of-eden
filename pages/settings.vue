3c40d75d134c18b2481ce29c133b88b2b51d074a
Charley Reeson <neon.fortytwo@gmail.com>
2024-08-10 14:08:09 +0100

    improve ux of settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -15,24 +15,30 @@
         <ul class="divide-y *:py-4">
           <li>
             Exclude hatchlings that aren't at least
-            <input
-              v-model.number="settings.hatchlingMinAge"
-              min="0"
-              max="72"
-              type="number"
-              class="w-20"
-            />
+            <span class="inline-flex flex-col mx-2">
+              <input
+                v-model.number="settings.hatchlingMinAge"
+                min="0"
+                max="72"
+                type="number"
+                class="w-20"
+              />
+              <span class="mt-1 text-xs text-center">(0 to 72)</span>
+            </span>
             hours old.
           </li>
           <li>
             Exclude eggs that aren't at least
-            <input
-              v-model.number="settings.eggMinAge"
-              min="0"
-              max="72"
-              type="number"
-              class="w-20"
-            />
+            <span class="inline-flex flex-col mx-2">
+              <input
+                v-model.number="settings.eggMinAge"
+                min="0"
+                max="72"
+                type="number"
+                class="w-20"
+              />
+              <span class="mt-1 text-xs text-center">(0 to 72)</span>
+            </span>
             hours old.
           </li>
         </ul>
@@ -54,7 +60,17 @@
           </li>
         </ul>
       </fieldset>
-      <button type="submit" class="btn-primary self-end">Save</button>
+      <button type="submit" class="btn-primary self-end" :disabled="canSave">
+        <font-awesome-icon
+          v-if="saveSettingsStatus === 'pending'"
+          :icon="['fas', 'spinner']"
+          class="animate-spin"
+        />
+        {{ saveSettingsStatus === 'pending' ? 'Saving...' : 'Save' }}
+      </button>
+      <p v-if="invalid" class="self-end">
+        Correct the values highlighted before saving.
+      </p>
     </form>
   </div>
 </template>
@@ -78,10 +94,26 @@ await useFetch('/api/user/settings', {
   },
 });
 
-const { execute: saveSettings } = useFetch('/api/user/settings', {
-  method: 'PATCH',
-  body: settings,
-  immediate: false,
-  watch: false,
+const invalid = computed(() => {
+  try {
+    userSettingsSchema.parse(settings.value);
+    return false;
+  } catch {
+    return true;
+  }
 });
+
+const canSave = computed(
+  () => saveSettingsStatus.value === 'pending' || invalid.value
+);
+
+const { execute: saveSettings, status: saveSettingsStatus } = useFetch(
+  '/api/user/settings',
+  {
+    method: 'PATCH',
+    body: settings,
+    immediate: false,
+    watch: false,
+  }
+);
 </script>
