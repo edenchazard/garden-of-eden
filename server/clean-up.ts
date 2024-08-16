bbf28af49225cdd6e812b3eca0e77c5ff2c1b882
Charley Reeson <neon.fortytwo@gmail.com>
2024-08-16 17:23:26 +0100

    HOTFIX: clean up script stall

--- server/clean-up.ts
+++ server/clean-up.ts
@@ -9,14 +9,18 @@ export async function cleanUp() {
 
   const con = await pool.getConnection();
 
-  const [codes] = await con.execute<RowDataPacket[]>(
-    `SELECT code FROM hatchery`
+  const [hatcheryDragons] = await con.execute<RowDataPacket[]>(
+    `SELECT code, in_seed_tray, in_garden FROM hatchery`
   );
 
-  const chunkedDragons = chunkArray(codes, 400);
-  const apiResponse = await Promise.all(
-    chunkedDragons.map((chunk) =>
-      $fetch<{
+  let overallRemoved = 0;
+  const chunkedDragons = chunkArray(hatcheryDragons, 400);
+
+  await Promise.all(
+    chunkedDragons.map(async (chunk) => {
+      const removeFromSeedTray = [];
+      const removeFromHatchery = [];
+      const apiResponse = await $fetch<{
         errors: unknown[];
         dragons: Record<string, DragonData>;
       }>(`https://dragcave.net/api/v2/dragons`, {
@@ -29,44 +33,61 @@ export async function cleanUp() {
         body: new URLSearchParams({
           ids: chunk.map((dragon) => dragon.code).join(','),
         }),
-      })
-    )
-  );
+      });
 
-  const removeFromSeedTray = apiResponse
-    .flatMap((response) => Object.values(response.dragons))
-    .filter((dragon) => dragon.hoursleft >= 96)
-    .map((dragon) => dragon.id);
+      for (const code in apiResponse.dragons) {
+        const dragon = apiResponse.dragons[code];
+        const hatcheryStatus = hatcheryDragons.find(
+          (d) => d.code === dragon.id
+        );
 
-  const toDelete = apiResponse
-    .flatMap((response) => Object.values(response.dragons))
-    .filter((dragon) => dragon.hoursleft < 0)
-    .map((dragon) => dragon.id);
+        if (
+          dragon.hoursleft < 0 ||
+          (hatcheryStatus?.in_seed_tray === 0 &&
+            hatcheryStatus?.in_garden === 0)
+        ) {
+          removeFromHatchery.push(code);
+        } else if (
+          hatcheryStatus?.in_seed_tray === 1 &&
+          dragon.hoursleft > 96
+        ) {
+          hatcheryStatus.in_seed_tray = 0;
+          removeFromSeedTray.push(code);
+        }
+      }
 
-  await con.beginTransaction();
+      await con.beginTransaction();
 
-  if (removeFromSeedTray.length) {
-    await con.execute(
-      con.format(`UPDATE hatchery SET in_seed_tray = 0 WHERE code IN (?)`, [
-        removeFromSeedTray,
-      ])
-    );
-  }
+      if (removeFromSeedTray.length) {
+        await con.execute(
+          con.format(`UPDATE hatchery SET in_seed_tray = 0 WHERE code IN (?)`, [
+            removeFromSeedTray,
+          ])
+        );
+      }
 
-  const [deleted] = await con.execute<ResultSetHeader>(
-    con.format(
-      `DELETE FROM hatchery WHERE code IN (?) OR (in_seed_tray = 0 AND in_garden = 0)`,
-      [toDelete.length ? toDelete : null]
-    )
+      if (removeFromHatchery.length) {
+        const [deleted] = await con.execute<ResultSetHeader>(
+          con.format(`DELETE FROM hatchery WHERE code IN (?)`, [
+            removeFromHatchery,
+          ])
+        );
+
+        overallRemoved += deleted.affectedRows;
+      }
+
+      await con.commit();
+      console.log('Deleting: ' + removeFromHatchery.join(', '));
+      console.log('Removing from seed tray: ', removeFromSeedTray.join(', '));
+    })
   );
 
   const end = new Date().getTime();
 
   await con.execute(
     `INSERT INTO recordings (value, record_type, extra) VALUES (?, ?, ?)`,
-    [deleted.affectedRows, 'removed', JSON.stringify({ start, end })]
+    [overallRemoved, 'removed', JSON.stringify({ start, end })]
   );
 
-  await con.commit();
   con.release();
 }
