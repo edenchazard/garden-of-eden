39f0fce6943389bf3246d235a5b8e04222a7cbcb
Charley Reeson <neon.fortytwo@gmail.com>
2024-10-03 23:00:26 +0100

    a

--- server/api/user/click.post.ts
+++ server/api/user/click.post.ts
@@ -5,7 +5,6 @@ import type { JWT } from 'next-auth/jwt';
 import { createSelectSchema } from 'drizzle-zod';
 
 export default defineEventHandler(async (event) => {
-  console.log(await readBody(event));
   const schema = createSelectSchema(hatcheryTable).pick({
     id: true,
   });
