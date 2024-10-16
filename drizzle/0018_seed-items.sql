9c605164e03ecbcdb22afe9edcbf940980ce90f9
Charley Reeson <neon.fortytwo@gmail.com>
2024-10-16 20:56:39 +0100

    add: even more flairs + replace anthurium

---
+++ drizzle/0018_seed-items.sql
@@ -0,0 +1,17 @@
+-- Custom SQL migration file, put you code below! --
+INSERT INTO items (name, url, category, cost, available_from, available_to) VALUES
+('Anthurium', '/anthurium.png', 'flair', 50, NULL, NULL),
+('Blackrose', '/blackrose.png', 'flair', 100, NULL, NULL),
+('Broccoli', '/broccoli.png', 'flair', 50, NULL, NULL),
+('Dragongrass', '/dragongrass.png', 'flair', 100, NULL, NULL),
+('Hibiscus', '/hibiscus.png', 'flair', 50, NULL, NULL),
+('Hyacinth', '/hyacinth.png', 'flair', 50, NULL, NULL),
+('Lily', '/lily.png', 'flair', 50, NULL, NULL),
+('Rafflesia', '/rafflesia.png', 'flair', NULL, NULL, NULL),
+('Saxifrage', '/saxifrage.png', 'flair', 50, NULL, NULL),
+('Sunflower', '/sunflower.png', 'flair', 100, NULL, NULL),
+('Dandelion', '/dandelion.png', 'flair', 50, NULL, NULL),
+('Sakura', '/sakura.png', 'flair', 50, NULL, NULL),
+('Black Calla Lily', '/black-calla-lily.png', 'flair', 100, NULL, NULL),
+('Witch Hazel', '/witch-hazel.png', 'flair', 50, '2024-10-01 00:00:00', '2024-11-07 00:00:00'),
+('Bird of Paradise', '/bird-of-paradise.png', 'flair', 50, NULL, NULL);
