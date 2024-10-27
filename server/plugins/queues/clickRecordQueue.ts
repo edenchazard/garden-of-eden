c22e463337d0e44ab13f837a299c6f92829da2b1
Dan Blackadder <danblackadder@gmail.com>
2024-10-27 16:20:45 +0000

    I think we there bro

---
+++ server/plugins/queues/clickRecordQueue.ts
@@ -0,0 +1,40 @@
+import { Worker as BullWorker } from 'bullmq';
+import { db } from '~/server/db';
+import { clicksTable, userTable } from '~/database/schema';
+import { and, eq, lt, sql } from 'drizzle-orm';
+
+const {
+  redis: { host, port },
+} = useRuntimeConfig();
+
+export default defineNitroPlugin(async () => {
+  new BullWorker(
+    'clickRecordQueue',
+    async (job) => {
+      const { userId, hatcheryId } = job.data;
+      const [clickRecord] = await db.insert(clicksTable).ignore().values({
+        hatchery_id: hatcheryId,
+        user_id: userId,
+      });
+
+      if (clickRecord.affectedRows === 0) {
+        return;
+      }
+
+      await db
+        .update(userTable)
+        .set({
+          money: sql`${userTable.money} + 1`,
+        })
+        .where(and(eq(userTable.id, userId), lt(userTable.money, 500)));
+    },
+    {
+      connection: {
+        host,
+        port: Number(port),
+      },
+    }
+  );
+
+  console.info('Click record queue worker started');
+});
---
+++ server/plugins/queues/shareScrollQueue.ts
@@ -0,0 +1,64 @@
+import { Worker as BullWorker } from 'bullmq';
+import { promises as fs, watch } from 'fs';
+import { shareScrollQueue } from '~/server/queue';
+import { Worker } from 'worker_threads';
+
+const {
+  redis: { host, port },
+  clientSecret,
+} = useRuntimeConfig();
+
+export default defineNitroPlugin(async () => {
+  await fs.mkdir('/cache/scroll', { recursive: true });
+
+  const createWorker = () =>
+    new Worker('./server/workers/shareScrollWorker.js', {
+      workerData: { username: null, filePath: null },
+    });
+
+  let shareScrollWorker = createWorker();
+
+  watch('./server/workers/shareScrollWorker.js', () => {
+    shareScrollWorker.terminate();
+    shareScrollWorker = createWorker();
+    console.info('Share scroll worker thread started');
+  });
+
+  shareScrollWorker
+    .on('message', async (message) => {
+      console.log('MESSAGE FROM WORKER', message);
+
+      if (message.type === 'error') {
+        await shareScrollQueue.removeDeduplicationKey(
+          `banner-${message.username}`
+        );
+      }
+    })
+    .on('error', (message) => {
+      console.log('error', message);
+    });
+
+  new BullWorker(
+    'shareScrollQueue',
+    async (job) => {
+      const { username, filePath } = job.data;
+      console.log('received', username, filePath);
+
+      shareScrollWorker.postMessage({
+        type: 'banner',
+        username,
+        filePath,
+        // TODO: change to user oauth token
+        token: clientSecret,
+      });
+    },
+    {
+      connection: {
+        host,
+        port: Number(port),
+      },
+    }
+  );
+
+  console.info('Banner worker queue started');
+});
