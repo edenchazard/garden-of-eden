723c22c8dfe7b10178b0d45daac483800a40e40d
edenchazard <38665888+edenchazard@users.noreply.github.com>
2024-11-02 00:32:47 +0000

    feat: clicks per day chart (#53)
    * feat: clicks per day chart
    
    * pretty fly for a ci guy

--- plugins/chartjs.ts
+++ plugins/chartjs.ts
@@ -25,7 +25,51 @@ export default defineNuxtPlugin(() => {
     Title,
     Tooltip,
     Legend,
-    Filler
+    Filler,
+    {
+      id: 'corsair',
+      defaults: {
+        width: 1,
+        color: 'rgba(255, 0, 255, 0.5)',
+        dash: [5, 5],
+      },
+      afterInit: (chart) => {
+        // @ts-expect-error I ain't dealing with that
+        chart.corsair = {
+          x: 0,
+          y: 0,
+        };
+      },
+      afterEvent: (chart, args) => {
+        const { inChartArea } = args;
+        const { x, y } = args.event;
+
+        // @ts-expect-error I ain't dealing with that
+        chart.corsair = { x, y, draw: inChartArea };
+        chart.draw();
+      },
+      afterDatasetsDraw: (chart, _, opts) => {
+        const { ctx } = chart;
+        const { top, bottom, left, right } = chart.chartArea;
+        // @ts-expect-error I ain't dealing with that
+        const { x, y, draw } = chart.corsair;
+        if (!draw) return;
+
+        ctx.save();
+
+        ctx.beginPath();
+        ctx.lineWidth = opts.width;
+        ctx.strokeStyle = opts.color;
+        ctx.setLineDash(opts.dash);
+        ctx.moveTo(x, bottom);
+        ctx.lineTo(x, top);
+        ctx.moveTo(left, y);
+        ctx.lineTo(right, y);
+        ctx.stroke();
+
+        ctx.restore();
+      },
+    }
   );
 
   Chart.defaults.maintainAspectRatio = false;
--- server/api/statistics/weekly.ts
+++ server/api/statistics/weekly.ts
@@ -1,22 +1,23 @@
 import {
   clicksLeaderboardTable,
+  clicksTable,
   itemsTable,
   userSettingsTable,
   userTable,
 } from '~/database/schema';
 import type { JWT } from 'next-auth/jwt';
 import { getToken } from '#auth';
-import { and, eq, lte, or, sql } from 'drizzle-orm';
+import { and, between, eq, lte, or, sql } from 'drizzle-orm';
 import { db } from '~/server/db';
 import { z } from 'zod';
-import { DateTime } from 'luxon';
+import { DateTime, Interval } from 'luxon';
 
 const clicksTotalForWeekCached = defineCachedFunction(
   async (start: DateTime) => {
-    const data = await db
+    const [{ clicks_this_week: clicks }] = await db
       .select({
         clicks_this_week:
-          sql<string>`SUM(${clicksLeaderboardTable.clicks_given})`.as(
+          sql<string>`0 + SUM(${clicksLeaderboardTable.clicks_given})`.as(
             'clicks_this_week'
           ),
       })
@@ -27,14 +28,50 @@ const clicksTotalForWeekCached = defineCachedFunction(
           eq(clicksLeaderboardTable.start, start.toJSDate())
         )
       );
-
-    return data;
+    return Number(clicks ?? 0);
   },
   {
     maxAge: 60 * 10,
     group: 'statistics',
     name: 'clickTotals',
     getKey: (start: DateTime) => `week-${start.toISO()}`,
+  }
+);
+
+const dailyTotalsForWeekCached = defineCachedFunction(
+  async (start: DateTime) => {
+    const data = await db
+      .select({
+        date: sql<string>`DATE(${clicksTable.clicked_on})`.as('date'),
+        clicks_given: sql<string>`COUNT(*)`.as('clicks_given'),
+      })
+      .from(clicksTable)
+      .where(
+        between(
+          clicksTable.clicked_on,
+          start.toJSDate(),
+          start.endOf('week').toJSDate()
+        )
+      )
+      .groupBy(sql`date`);
+
+    return Object.fromEntries(
+      Interval.fromDateTimes(start, start.endOf('week'))
+        .splitBy({
+          days: 1,
+        })
+        .map((date: Interval) => [
+          date.start?.toISODate(),
+          data.find((d) => d.date === date.start?.toISODate())?.clicks_given ??
+            0,
+        ])
+    ) as Record<string, number>;
+  },
+  {
+    maxAge: 60 * 10,
+    group: 'statistics',
+    name: 'clickTotals',
+    getKey: (start: DateTime) => `week-${start.toISO()}-daily-totals`,
   }
 );
 
@@ -48,8 +85,9 @@ export default defineEventHandler(async (event) => {
 
   const weekStart = DateTime.fromISO(query.start);
 
-  const [[{ clicks_this_week: clicksGiven }], results] = await Promise.all([
+  const [clicksGiven, dailyTotals, results] = await Promise.all([
     clicksTotalForWeekCached(weekStart),
+    dailyTotalsForWeekCached(weekStart),
     db
       .select({
         rank: clicksLeaderboardTable.rank,
@@ -88,7 +126,8 @@ export default defineEventHandler(async (event) => {
 
   return {
     results,
-    clicksGiven: parseInt(clicksGiven),
+    clicksGiven,
+    dailyTotals,
     weekStart: weekStart.toISO() as string,
     weekEnd: weekStart.plus({ days: 7 }).toISO() as string,
   };
