bdabce52215a843ef32645c36eeca5f4ff1b96c4
Charley Reeson <neon.fortytwo@gmail.com>
2024-11-05 22:05:45 +0000

    add flairs: buddleia and violet

---
+++ drizzle/0024_seed-buddleia-violet.sql
@@ -0,0 +1,4 @@
+-- Custom SQL migration file, put you code below! --
+INSERT INTO items (name, url, category, cost, available_from, available_to, artist, description) VALUES
+('Buddleia', 'buddleia.png', 'flair', 150, NULL, NULL, "inghelene", "Buddleia produce thousands of seeds and can even grow in concrete. That won't be happening with this one."),
+('Violet', 'violet.png', 'flair', 250, NULL, NULL, "04uni", "A popular flower, violets symbolise innocence, modesty and everlasting love.");
