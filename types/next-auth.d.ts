4b45f0bb149e0f75ecf008959e9840a451a4236b
Charley Reeson <neon.fortytwo@gmail.com>
2024-11-15 23:20:01 +0000

    add to share page

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -14,6 +14,7 @@ declare module 'next-auth/jwt' {
 declare module 'next-auth' {
   interface Session {
     user: {
+      id: number;
       username: string;
       role: UserRole;
       settings: UserSettings;
