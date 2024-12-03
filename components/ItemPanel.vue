bd8ea8e0527b94850f4f4e384ca22953b9472055
Charley Reeson <neon.fortytwo@gmail.com>
2024-12-03 11:38:32 +0000

    add: artist credits to tooltips

--- components/ItemPanel.vue
+++ components/ItemPanel.vue
@@ -6,6 +6,7 @@
       <div class="w-full max-w-56 p-2">
         <b class="font-bold">{{ item.name }}</b>
         <div class="text-sm" v-html="item.description" />
+        <p class="text-xs text-right italic">Art by {{ item.artist }}</p>
       </div>
     </template>
   </VDropdown>
