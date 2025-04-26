08ff5aecb35ef1bde9917300270904583e3c045f
Charley Reeson <neon.fortytwo@gmail.com>
2025-04-26 23:11:41 +0100

    aaaaand another

--- drizzle/0038_flippant_rumiko_fujikawa.sql
+++ drizzle/0038_flippant_rumiko_fujikawa.sql
@@ -3,4 +3,4 @@ RENAME TABLE `banner_jobs` TO `user_banner_job`;--> statement-breakpoint
 RENAME TABLE `purchases` TO `user_item`;--> statement-breakpoint
 RENAME TABLE `user_notifications` TO `user_notification`;--> statement-breakpoint
 RENAME TABLE `users_trophies` TO `user_trophy`;--> statement-breakpoint
-RENAME TABLE `user_settings` TO `users_settings`;--> statement-breakpoint
+RENAME TABLE `user_settings` TO `users_settings`;
