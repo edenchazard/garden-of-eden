10df3629a025d0e11a0091449c1ce90000cc95d7
edenchazard <38665888+edenchazard@users.noreply.github.com>
2025-06-14 18:40:08 +0100

    fix radio alignment, toggle shrink (#125)

--- components/ToggleInput.vue
+++ components/ToggleInput.vue
@@ -2,7 +2,7 @@
   <label class="inline-flex items-center cursor-pointer">
     <input v-model="model" type="checkbox" class="sr-only peer" />
     <div
-      class="relative bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:transition-all dark:border-gray-600 peer-checked:bg-orange-200 dark:peer-checked:bg-sky-900"
+      class="shrink-0 relative bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:transition-all dark:border-gray-600 peer-checked:bg-orange-200 dark:peer-checked:bg-sky-900"
       :class="{
         'w-7 h-2 after:size-3 after:-top-[2px]': size === 'sm',
         'w-11 h-6 after:size-5 after:top-[2px]': size === 'md',
