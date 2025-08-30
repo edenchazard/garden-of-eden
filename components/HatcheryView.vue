c13e0ef96163a51432f9bcd3979adcc1a3ce5cf2
Charley Reeson <neon.fortytwo@gmail.com>
2025-08-30 18:12:28 +0100

    watering can cursor: standing when hovering, tilt when active

--- components/HatcheryView.vue
+++ components/HatcheryView.vue
@@ -92,7 +92,7 @@
     <a
       v-for="dragon in hatchery.dragons"
       :key="dragon.id"
-      class="size-full flex items-center justify-center cursor-watering-can"
+      class="size-full flex items-center justify-center cursor-watering-can active:cursor-watering-can-tilted"
       :class="{
         'bg-orange-100/30 dark:bg-sky-200/15 transition-colors':
           dragon.clickedOn && highlightClickedDragons,
