7beb26b16d770afef56cbca0ace428d088fa3a4e
Charley Reeson <neon.fortytwo@gmail.com>
2025-09-07 16:26:26 +0100

    revert

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'drizzle-kit';
 
+const { db } = useRuntimeConfig();
+
 export default defineConfig({
   dialect: 'mysql',
   schema: './database/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    user: process.env.NUXT_DB_USER || '',
-    password: process.env.NUXT_DB_PASSWORD || '',
-    host: process.env.NUXT_DB_HOST || 'localhost',
-    port: parseInt(process.env.NUXT_DB_PORT || '3306'),
-    database: process.env.NUXT_DB_DATABASE || '',
+    user: db.user,
+    password: db.password,
+    host: db.host,
+    port: 3306,
+    database: db.database,
   },
 });
