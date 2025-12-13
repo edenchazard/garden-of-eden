0d441008e0b94d6af18f8a013cad2d2b83b6b4be
edenchazard <38665888+edenchazard@users.noreply.github.com>
2025-12-13 15:50:36 +0000

    matthias rides his sleigh for christmas, powered by pargulus pygmies. (#157)

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -65,17 +65,8 @@
         <div class="flex gap-y-1 flex-col sm:flex-row justify-between">
           <div class="text-center z-10">
             <span class="inline-block relative bg-(--outer-bg)">
-              <img
-                v-if="
-                  Interval.fromDateTimes(
-                    DateTime.fromISO(`${DateTime.now().year}-10-24T00:00:00Z`),
-                    DateTime.fromISO(`${DateTime.now().year}-11-07T00:00:00Z`)
-                  ).contains(DateTime.utc())
-                "
-                src="/npc/ghost_of_eden.webp"
-                alt=""
-                class="ghost-of-eden absolute -z-10 left-5"
-              />
+              <GhostOfEden />
+              <SantaMatthias />
               <NuxtLink
                 to="/"
                 :style="{
@@ -175,8 +166,9 @@
 </template>
 
 <script lang="ts" setup>
-import { DateTime, Interval } from 'luxon';
 import itemUrl from '~/utils/itemUrl';
+import SantaMatthias from '~/components/SantaMatthias.vue';
+import GhostOfEden from '~/components/GhostOfEden.vue';
 
 useHead({
   titleTemplate(titleChunk) {
@@ -199,50 +191,3 @@ const { execute: cleanUp } = useFetch('/api/hatchery', {
   body: {},
 });
 </script>
-
-<style scoped>
-.ghost-of-eden {
-  animation: ghost-of-eden 10s infinite alternate 2s;
-}
-
-@keyframes ghost-of-eden {
-  0% {
-    opacity: 0;
-    transform: translateY(0);
-  }
-  20% {
-    opacity: 1;
-    transform: translateY(-2rem);
-  }
-  30% {
-    opacity: 1;
-    transform: translateY(-1.8rem);
-  }
-  40% {
-    opacity: 1;
-    transform: translateY(-2rem);
-  }
-  50% {
-    opacity: 1;
-    transform: translateY(-1.8rem);
-  }
-  60% {
-    opacity: 1;
-    transform: translateY(-2rem);
-  }
-  75% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-  90% {
-    opacity: 0;
-    transform: translateY(0);
-  }
-}
-
-@media (prefers-reduced-motion: reduce) {
-  .ghost-of-eden {
-    animation: none;
-  }
-}
-</style>
