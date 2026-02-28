8da9f53057dd4d1ff5627d7bbb4ffac03b6287a4
edenchazard <neon.fortytwo@gmail.com>
2026-02-28 17:11:42 +0000

    test shop page

--- test/e2e/index.spec.ts
+++ test/e2e/index.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest';
 import { createPage, setup } from '@nuxt/test-utils/e2e';
 
 describe('Homepage', async () => {
-  await setup({});
+  await setup();
 
   describe('when user is logged out', () => {
     test('user sees intro and sign in button', async () => {
@@ -34,6 +34,24 @@ describe('Homepage', async () => {
 
       await signInButton.click();
       await page.waitForURL(/https:\/\/dragcave.net\/login/);
+    });
+
+    test('user can see the shop page and at least one item', async () => {
+      const page = await createPage('/shop');
+
+      const explainer = page
+        .locator('p')
+        .filter({
+          hasText:
+            'Matthias will sell you flairs to display alongside your username in exchange for Dragon Dollars, earned by clicking. You must be logged in.',
+        })
+        .first();
+
+      expect(await explainer.isVisible()).to.be.true;
+
+      const shopItems = page.locator('[data-shop-item]');
+
+      expect(await shopItems.count()).to.be.greaterThan(0);
     });
   });
 });
